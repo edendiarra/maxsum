03ace51bd66f22cba0375f6e89f5de06713736ea
193 <193@DESKTOP-DLOI120>
2017-01-16 14:51:58 +0900

    max first

---
+++ max/src/test/maxtest.java
@@ -0,0 +1,34 @@
+package test;
+
+public class maxtest {
+
+	public static void main(String[] args) 
+	{
+		int[]nums={57,5,10,7,63,9};
+		sort(nums,1);
+		sort(nums,2);
+
+	}
+	private static void sort(int[]nums,int check)
+	{
+		int temp=0;
+		if(check==1)
+		{
+			for(int i=0;i<nums.length;i++)
+			{
+				for(int j=i+1;j<nums.length;j++)
+				{
+					if(nums[i]>nums[j])
+					{
+						temp=nums[j];
+						nums[j]=nums[i];
+						nums[i]=temp;					
+					}
+				}
+			}
+			System.out.print("�ִ밪="+nums[5]);	
+			System.out.println("");
+		}
+
+	}
+}
