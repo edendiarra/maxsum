f45314c558d5e8d5a776a0894d538023f5a7a8ad
edendiarra <only1kr@naver.com>
2017-02-22 14:38:47 +0900

    Update maxtest.java

--- max/src/test/maxtest.java
+++ max/src/test/maxtest.java
@@ -4,50 +4,17 @@ public class maxtest {
 
 	public static void main(String[] args) 
 	{
-		int[]nums={57,5,10,7,63,9};
-		sort(nums,1);
-		sort(nums,2);
+		int a=10;
+		int b=20;
 
 	}
 	private static void sort(int[]nums,int check)
 	{
 		int temp=0;
-		if(check==1)
-		{
-			for(int i=0;i<nums.length;i++)
-			{
-				for(int j=i+1;j<nums.length;j++)
-				{
-					if(nums[i]>nums[j])
-					{
-						temp=nums[j];
-						nums[j]=nums[i];
-						nums[i]=temp;					
-					}
-				}
-			}
-			System.out.print("최대값"+nums[5]);	
-			System.out.println("");
-		}
-		else
-		{
-			for(int i=0;i<nums.length;i++)
-			{
-				for(int j=i+1;j<nums.length;j++)
-				{
-					if(nums[i]<nums[j])
-					{
-						temp=nums[j];
-						nums[j]=nums[i];
-						nums[i]=temp;					
-					}
-				}
-			}
+		a+b=temp;
+	System.out.print("합="+temp);
+	}		
 
-			System.out.print("최소값"+nums[5]);
+	
 
-			
-		}
-
-	}
 }
